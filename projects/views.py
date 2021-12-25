e47b259384d0357628b63c33152b6a78306bb094
neerajsinghal933 <neerajsinghal933@gmail.com>
2021-12-25 19:27:34 +0530

    first commit

---
+++ projects/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+from django.http import HttpResponse
+
+
+def projects(request):
+    page = "projects"
+    number = 11
+    context = {"page": page, "number": number}
+    return render(request, "projects/projects.html", context)
+
+
+def project(request, pk):
+    return render(request, "projects/single-project.html")
